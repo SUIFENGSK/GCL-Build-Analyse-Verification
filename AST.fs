b101995a18d8d2dde35665ed526cb8ee66e2c4a1
Shuokai Ma <kkm981104@gmail.com>
2023-03-02 21:04:32 +0100

    Task 1 (Done with AST, and begin with parser)

--- AST.fs
+++ AST.fs
@@ -13,3 +13,49 @@ type expr =
     | UMinusExpr of (expr)
     | RootExpr of (expr)
     | LogExpr of (expr)
+
+// Task 1
+// a  ::=  n  |  x  |  A[a]  |  a + a  |  a - a  |  a * a  |  a / a  |  - a  |  a ^ a  |  (a)
+type arithmeticExpr = 
+    | Num of float
+    | Str of string
+    | ArrAccess of (string * arithmeticExpr)
+    | PlusExpr of (arithmeticExpr * arithmeticExpr)
+    | MinusExpr of (arithmeticExpr * arithmeticExpr)
+    | TimesExpr of (arithmeticExpr * arithmeticExpr)
+    | DivExpr of (arithmeticExpr * arithmeticExpr)
+    | UPlusExpr of (arithmeticExpr)
+    | UMinusExpr of (arithmeticExpr)
+    | PowExpr of (arithmeticExpr * arithmeticExpr)
+    | ParenExpr of (arithmeticExpr)
+
+// b  ::=  true  |  false  |  b & b  |  b | b  |  b && b  |  b || b  |  ! b
+//    |  a = a  |  a != a  |  a > a  |  a >= a  |  a < a  |  a <= a  |  (b)
+type booleanExpr = 
+    | True
+    | False
+    | AndExpr of (booleanExpr * booleanExpr)
+    | OrExpr of (booleanExpr * booleanExpr)
+    | AndAndExpr of (booleanExpr * booleanExpr)
+    | OrOrExpr of (booleanExpr * booleanExpr)
+    | NotExpr of (booleanExpr)
+    | EqExpr of (arithmeticExpr * arithmeticExpr)
+    | NeqExpr of (arithmeticExpr * arithmeticExpr)
+    | GtExpr of (arithmeticExpr * arithmeticExpr)
+    | GteExpr of (arithmeticExpr * arithmeticExpr)
+    | LtExpr of (arithmeticExpr * arithmeticExpr)
+    | LteExpr of (arithmeticExpr * arithmeticExpr)
+    | ParenExpr of (booleanExpr)
+
+// C  ::=  x := a  |  A[a] := a  |  skip  |  C ; C  |  if GC fi  |  do GC od
+// GC ::=  b -> C  |  GC [] GC
+type command = 
+    | Assign of (string * arithmeticExpr)
+    | ArrAssign of (string * arithmeticExpr * arithmeticExpr)
+    | Skip
+    | Sequence of (command * command)
+    | If of guardedCommand
+    | Do of guardedCommand
+and guardedCommand =
+    | Condition of (booleanExpr * command)
+    | Choice of (guardedCommand * guardedCommand)
