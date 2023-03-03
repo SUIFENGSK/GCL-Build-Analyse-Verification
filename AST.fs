db0fc589cb35f6a29bf4e9e1ac5ae0317b200d0f
Shuokai Ma <kkm981104@gmail.com>
2023-03-03 19:50:22 +0100

    All cmds work now!

--- AST.fs
+++ AST.fs
@@ -19,21 +19,21 @@ type arithmeticExpr =
 
 // b  ::=  true  |  false  |  b & b  |  b | b  |  b && b  |  b || b  |  ! b
 //    |  a = a  |  a != a  |  a > a  |  a >= a  |  a < a  |  a <= a  |  (b)
-// type booleanExpr = 
-//     | True
-//     | False
-//     | AndExpr of (booleanExpr * booleanExpr)
-//     | OrExpr of (booleanExpr * booleanExpr)
-//     | AndAndExpr of (booleanExpr * booleanExpr)
-//     | OrOrExpr of (booleanExpr * booleanExpr)
-//     | NotExpr of (booleanExpr)
-//     | EqExpr of (arithmeticExpr * arithmeticExpr)
-//     | NeqExpr of (arithmeticExpr * arithmeticExpr)
-//     | GtExpr of (arithmeticExpr * arithmeticExpr)
-//     | GteExpr of (arithmeticExpr * arithmeticExpr)
-//     | LtExpr of (arithmeticExpr * arithmeticExpr)
-//     | LteExpr of (arithmeticExpr * arithmeticExpr)
-//     | ParenBExpr of (booleanExpr)
+type booleanExpr = 
+    | True
+    | False
+    | AndExpr of (booleanExpr * booleanExpr)
+    | OrExpr of (booleanExpr * booleanExpr)
+    | AndAndExpr of (booleanExpr * booleanExpr)
+    | OrOrExpr of (booleanExpr * booleanExpr)
+    | NotExpr of (booleanExpr)
+    | EqExpr of (arithmeticExpr * arithmeticExpr)
+    | NeqExpr of (arithmeticExpr * arithmeticExpr)
+    | GtExpr of (arithmeticExpr * arithmeticExpr)
+    | GteExpr of (arithmeticExpr * arithmeticExpr)
+    | LtExpr of (arithmeticExpr * arithmeticExpr)
+    | LteExpr of (arithmeticExpr * arithmeticExpr)
+    | ParenBExpr of (booleanExpr)
 
 // C  ::=  x := a  |  A[a] := a  |  skip  |  C ; C  |  if GC fi  |  do GC od
 // GC ::=  b -> C  |  GC [] GC
@@ -42,12 +42,14 @@ type command =
     | ArrAssign of (string * arithmeticExpr * arithmeticExpr)
     | Skip
     | Seq of (command * command)
-//     | If of guardedCommand
-//     | Do of guardedCommand
-// and guardedCommand =
-//     | Condition of (booleanExpr * command)
-//     | Choice of (guardedCommand * guardedCommand)
+    | If of guardedCommand
+    | Do of guardedCommand
+and guardedCommand =
+    | Condition of (booleanExpr * command)
+    | Choice of (guardedCommand * guardedCommand)
 
 type AST = 
     | A of arithmeticExpr
     | C of command
+    | GC of guardedCommand
+    | B of booleanExpr
--- Parse.fs
+++ Parse.fs
@@ -34,20 +34,72 @@ let rec prettyPrint (ast:AST) =
         | ArrAssign (s, a1, a2) -> s + "[" + prettyPrint (A a1) + "] := " + prettyPrint (A a2)
         | Skip -> "skip"
         | Seq (c1, c2) -> prettyPrint (C c1) + "; \n" + prettyPrint (C c2)
+        | If gc -> "if " + prettyPrintGC gc + " fi"
+        | Do gc -> "do " + prettyPrintGC gc + " od"
+    and prettyPrintGC (gc:guardedCommand) =
+        match gc with
+        | Condition (b, c) -> prettyPrint (B b) + " -> " + prettyPrint (C c)
+        | Choice (gc1, gc2) -> prettyPrintGC gc1 + " [] " + prettyPrintGC gc2
         
     let rec prettyPrintAExpr (a:arithmeticExpr) = 
         match a with
         | Num n -> string n
         | Str s -> string s
+        | ArrAccess (s, a) -> s + "[" + prettyPrint (A a) + "]"
+        | PlusExpr (a1, a2) -> prettyPrint (A a1) + " + " + prettyPrint (A a2)
+        | MinusExpr (a1, a2) -> prettyPrint (A a1) + " - " + prettyPrint (A a2)
+        | TimesExpr (a1, a2) -> prettyPrint (A a1) + " * " + prettyPrint (A a2)
+        | DivExpr (a1, a2) -> prettyPrint (A a1) + " / " + prettyPrint (A a2)
+        | UPlusExpr a -> "+" + prettyPrint (A a)
+        | UMinusExpr a -> "-" + prettyPrint (A a)
+        | PowExpr (a1, a2) -> prettyPrint (A a1) + " ^ " + prettyPrint (A a2)
+        | ParenAExpr a -> "(" + prettyPrint (A a) + ")"
+    
+    let rec prettyPrintBExpr (b:booleanExpr) = 
+        match b with
+        | True -> "true"
+        | False -> "false"
+        | AndExpr (b1, b2) -> prettyPrint (B b1) + " & " + prettyPrint (B b2)
+        | OrExpr (b1, b2) -> prettyPrint (B b1) + " | " + prettyPrint (B b2)
+        | AndAndExpr (b1, b2) -> prettyPrint (B b1) + " && " + prettyPrint (B b2)
+        | OrOrExpr (b1, b2) -> prettyPrint (B b1) + " || " + prettyPrint (B b2)
+        | NotExpr b -> "!" + prettyPrint (B b)
+        | EqExpr (a1, a2) -> prettyPrint (A a1) + " = " + prettyPrint (A a2)
+        | NeqExpr (a1, a2) -> prettyPrint (A a1) + " != " + prettyPrint (A a2)
+        | GtExpr (a1, a2) -> prettyPrint (A a1) + " > " + prettyPrint (A a2)
+        | GteExpr (a1, a2) -> prettyPrint (A a1) + " >= " + prettyPrint (A a2)
+        | LtExpr (a1, a2) -> prettyPrint (A a1) + " < " + prettyPrint (A a2)
+        | LteExpr (a1, a2) -> prettyPrint (A a1) + " <= " + prettyPrint (A a2)
+        | ParenBExpr b -> "(" + prettyPrint (B b) + ")"
 
     match ast with
     | C c -> prettyPrintCmd c
     | A a -> prettyPrintAExpr a
+    | B b -> prettyPrintBExpr b
 
 
 let analysis (src: string) : string =
-    match parse Parser.startCommand (src) with
+    // try to parse arithmetic expression
+    match parse Parser.start (src) with
         | Ok ast ->
             Console.Error.WriteLine("> {0}", ast)
-            prettyPrint (C ast)
-        | Error e -> "Parse error: {0}"
+            prettyPrint (A ast)
+        | Error e -> 
+    // try to parse boolean expression
+    match parse Parser.startBooleanExpr (src) with
+                        | Ok ast ->
+                            Console.Error.WriteLine("> {0}", ast)
+                            prettyPrint (B ast)
+                        | Error e ->  
+    // try to parse command
+    match parse Parser.startCommand (src) with
+                        | Ok ast ->
+                            Console.Error.WriteLine("> {0}", ast)
+                            prettyPrint (C ast)
+                        | Error e -> 
+    // try to parse guarded command
+    match parse Parser.startGuardedCommand (src) with
+                        | Ok ast ->
+                            Console.Error.WriteLine("> {0}", ast)
+                            prettyPrint (GC ast)
+                        | Error e -> "Parse error: {0}" + e.Message
